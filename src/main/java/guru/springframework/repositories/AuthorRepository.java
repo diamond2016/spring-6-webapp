8f8c841e3eb6765302c81f5041c792486dcdb584
Riccardo Corsi <riccardogcorsi@gmail.com>
2025-08-28 09:27:22 +0200

    create interface for repositories of entities Book 6 author

---
+++ src/main/java/guru/springframework/repositories/AuthorRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import guru.springframework.domain.Author;
+
+public interface AuthorRepository extends CrudRepository<Author, Long>{
+
+}
