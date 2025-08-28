686297bfba4cdcdac354546491c8e12a3a455047
Riccardo Corsi <riccardogcorsi@gmail.com>
2025-08-28 12:41:08 +0200

    tree of packeges

--- src/main/java/guru/springframework/repositories/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package guru.springframework.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import guru.springframework.domain.Author;
-
-public interface AuthorRepository extends CrudRepository<Author, Long>{
-
-}
