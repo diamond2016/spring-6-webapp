3322f2209bd681dfa5a3de0c097d421d5e5d5a61
Riccardo Corsi <riccardogcorsi@gmail.com>
2025-08-31 14:14:23 +0200

    solved pom.xml and adding simple style books.html

--- src/main/java/guru/springframework/spring6webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring6webapp/controllers/BookController.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6webapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import guru.springframework.spring6webapp.services.BookService;
 
@@ -13,8 +14,9 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    @RequestMapping("/books")
     public String getBooks(Model model) {
-        model.addAttribute("books", bookService.findAll()); // map books -> iterable of info
+        model.addAttribute("books", bookService.findAll()); // call a view named: books (map key books -> iterable of info)
         return "books";
     }
 }
